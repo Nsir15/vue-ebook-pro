501746883091fc3d90f65380a0fb644f430ba76a
南希 <447108518@qq.com>
2020-06-28 22:44:46 +0800

    初始化 epubjs

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -1,27 +1,45 @@
 <template>
- <div id="reader">
-{{$route.params.filename}}
+ <div id="epub-reader">
+   <div id="reader">
+
+   </div>
  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import Epub from 'epubjs'
+
 export default {
-  components: {},
+  components: {
+  },
   props: {},
   data () {
     return {
 
     }
   },
-  computed: {},
-  created () {
-
+  computed: {
+    ...mapGetters(['fileName'])
   },
   mounted () {
-
+    const fileName = this.$route.params.filename.split('|').join('/')
+    this.$store.dispatch('setFileName', fileName).then(() => {
+      this.initEpub()
+    })
   },
   methods: {
-
+    initEpub () {
+      const baseUrl = 'http://192.168.1.3:9001/epub/'
+      const url = baseUrl + this.fileName + '.epub'
+      this.book = new Epub(url)
+      this.rendition = this.book.renderTo('reader', {
+        width: innerWidth,
+        height: innerHeight,
+        method: 'default'
+      })
+      this.rendition.display()
+    }
   }
 }
 </script>
