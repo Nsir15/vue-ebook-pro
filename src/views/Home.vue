64e3a871dcc22f46aeac83d68e6ac2f752945027
南希 <447108518@qq.com>
2020-06-28 14:41:24 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,14 @@
 <script>
 import { mapGetters } from 'vuex'
 
+//  假设这里就是我们在vuex 的getter中定义的属性
 const getters = {
   a: () => 1,
   b: () => 2,
   c: () => 3
 }
+//  这个对应的是 导入的mapGetters
+//  该函数返回一个对象
 function fn (keys) {
   const data = {}
   keys.forEach(key => {
@@ -31,6 +34,7 @@ export default {
 
     }
   },
+  // 在计算属性中。通过ES6的展开运算符，将不同的对象进行合并
   computed: {
     ...mapGetters(['test']),
     ...fn(['a', 'b', 'c'])
