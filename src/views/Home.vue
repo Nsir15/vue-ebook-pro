1e892b7acaf6079f37d2be28124e1134c714ccc9
南希 <447108518@qq.com>
2020-06-28 14:13:14 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,34 @@
 <template>
-  <div class="home">
-    <div id="ebook"></div>
-  </div>
+ <div>
+
+ </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Epub from 'epubjs'
-
 export default {
-  name: 'Home',
-  components: {
+  name: '',
+  components: {},
+  props: {},
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
   }
 }
 </script>
+
+<style lang="scss" scoped >
+
+</style>
