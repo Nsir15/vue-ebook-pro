f135fc3445fa7cfbaa373d7c0486b0a8f5183992
南希 <447108518@qq.com>
2020-06-28 14:32:25 +0800

    引用vuex,分模块。 模拟 实现 mapGetter 的实现原理

--- src/store/modules/book.js
+++ src/store/modules/book.js
@@ -1,9 +1,16 @@
 const book = {
   state: {
+    test: 'bookTest'
   },
   mutations: {
+    SET_TEST: (state, newTest) => {
+      state.test = newTest
+    }
   },
   actions: {
+    setTest: ({ commit, state }, newTest) => {
+      return commit('SET_TEST', newTest)
+    }
   }
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,23 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+const getters = {
+  a: () => 1,
+  b: () => 2,
+  c: () => 3
+}
+function fn (keys) {
+  const data = {}
+  keys.forEach(key => {
+    if (Object.prototype.hasOwnProperty.call(getters, key)) {
+      data[key] = getters[key]
+    }
+  })
+  return data
+}
+
 export default {
   name: '',
   components: {},
@@ -15,13 +32,16 @@ export default {
     }
   },
   computed: {
-
+    ...mapGetters(['test']),
+    ...fn(['a', 'b', 'c'])
   },
   created () {
 
   },
   mounted () {
-
+    console.log(this.test)
+    console.log(this.a, this.b, this.c)
+    console.log(this.d)
   },
   methods: {
 
