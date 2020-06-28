ec95333a086388fd4d6d187e36f03228b075e436
南希 <447108518@qq.com>
2020-06-28 13:54:51 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <div id="ebook"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Epub from 'epubjs'
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
