3f9e7567367a661ecb242edfc3b6a3c6b5d37158
南希 <447108518@qq.com>
2020-06-29 21:52:32 +0800

    popup 添加transition动画

--- src/components/ebook/EbookFontFamilyPopup.vue
+++ src/components/ebook/EbookFontFamilyPopup.vue
@@ -1,20 +1,28 @@
 <template>
- <div class="font-family-popup" v-show="fontFamilyVisible">
-   <div class="font-family-popup-title">
-     <div class="popup-title-icon" @click="dismissPopup">
-       <span class="icon-down2"></span>
-     </div>
-     <div class="popup-title-text">设置字体</div>
-   </div>
-   <div class="font-family-popup-list">
-     <div class="font-family-list-item" v-for="(item,index) in fontFamilyList" :key="index" @click="setFontFamily(item.font)">
-       <div class="font-family-list-item-text" :class="{'selected':item.font === defaultFontFamily}">{{item.font}}</div>
-       <div class="font-family-list-item-icon" v-if="item.font === defaultFontFamily">
-         <span class="icon-check"></span>
-       </div>
-     </div>
-   </div>
- </div>
+  <transition name="popup-slide-up">
+    <div class="font-family-popup" v-show="fontFamilyVisible">
+      <div class="font-family-popup-title">
+        <div class="popup-title-icon" @click="dismissPopup">
+          <span class="icon-down2"></span>
+        </div>
+        <div class="popup-title-text">设置字体</div>
+      </div>
+      <div class="font-family-popup-list">
+        <div
+          class="font-family-list-item"
+          v-for="(item,index) in fontFamilyList"
+          :key="index"
+          @click="setFontFamily(item.font)">
+          <div
+            class="font-family-list-item-text"
+            :class="{'selected':item.font === defaultFontFamily}">{{item.font}}</div>
+          <div class="font-family-list-item-icon" v-if="item.font === defaultFontFamily">
+            <span class="icon-check"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -31,12 +39,8 @@ export default {
     }
   },
   computed: {},
-  created () {
-
-  },
-  mounted () {
-
-  },
+  created () {},
+  mounted () {},
   methods: {
     dismissPopup () {
       this.setFontFamilyVisible(false)
@@ -49,24 +53,24 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import '../../assets/styles/global';
+@import "../../assets/styles/global";
 
-.font-family-popup{
-  position:absolute;
+.font-family-popup {
+  position: absolute;
   left: 0;
   bottom: 0;
   width: 100%;
   // height: px2rem(200);
   background: white;
-  box-shadow: 0 px2rem(-8)  px2rem(8)  rgba(0, 0, 0, 0.15);
+  box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
   z-index: 120;
-  .font-family-popup-title{
+  .font-family-popup-title {
     position: relative;
     padding: px2rem(15);
     box-sizing: border-box;
     border-bottom: px2rem(1) solid #ccc;
     @include center;
-    .popup-title-icon{
+    .popup-title-icon {
       position: absolute;
       left: px2rem(15);
       top: 0;
@@ -75,26 +79,26 @@ export default {
       font-weight: bolder;
       @include center;
     }
-    .popup-title-text{
+    .popup-title-text {
       font-size: px2rem(14);
       font-weight: bolder;
     }
   }
-  .font-family-popup-list{
+  .font-family-popup-list {
     padding: px2rem(10);
-    .font-family-list-item{
+    .font-family-list-item {
       display: flex;
       padding: px2rem(15);
       // @include center;
-      .font-family-list-item-text{
+      .font-family-list-item-text {
         flex: 1;
         font-size: px2rem(15);
-        &.selected{
+        &.selected {
           color: aqua;
           font-weight: bolder;
         }
       }
-      .font-family-list-item-icon{
+      .font-family-list-item-icon {
         flex: 1;
         color: aqua;
         text-align: right;
@@ -104,5 +108,4 @@ export default {
     }
   }
 }
-
 </style>
