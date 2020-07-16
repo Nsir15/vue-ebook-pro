980bc7aaeb5f6fd2409ace0af7ec93a352459936
南希 <447108518@qq.com>
2020-07-16 20:50:02 +0800

    书城首页的基本框架

---
+++ src/views/store/StoreHome.vue
@@ -0,0 +1,44 @@
+<!--
+ * @Description:
+ * @Author: MRNAN
+ * @Date: 2020-07-16 20:45:20
+ * @LastEditTime: 2020-07-16 20:49:19
+ * @LastEditors: MRNAN
+ * @FilePath: /Vue-ebook-pro/src/views/store/StoreHome.vue
+-->
+<template>
+ <div class="store-home">
+home
+ </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+
+    }
+  },
+  computed: {},
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../assets/styles/global';
+.store-home{
+  width: 100%;
+  height: 100%;
+  background: white;
+}
+</style>
---
+++ src/views/store/index.vue
@@ -0,0 +1,42 @@
+<!--
+ * @Description:书城首页
+ * @Author: MRNAN
+ * @Date: 2020-07-16 10:11:46
+ * @LastEditTime: 2020-07-16 20:49:07
+ * @LastEditors: MRNAN
+ * @FilePath: /Vue-ebook-pro/src/views/store/index.vue
+-->
+<template>
+ <div class="store">
+   <router-view></router-view>
+ </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+
+    }
+  },
+  computed: {},
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.store{
+  width: 100%;
+  height: 100%;
+}
+</style>
