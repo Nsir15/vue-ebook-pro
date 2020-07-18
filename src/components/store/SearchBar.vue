fe433440a1614699dd794bdbc2b601e1e46f53b2
南希 <447108518@qq.com>
2020-07-18 17:50:57 +0800

    通过flex 和 占位 实现Input缩放；back高度变化垂直居中

--- src/components/store/SearchBar.vue
+++ src/components/store/SearchBar.vue
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: MRNAN
  * @Date: 2020-07-16 20:51:21
- * @LastEditTime: 2020-07-18 17:26:31
+ * @LastEditTime: 2020-07-18 17:50:11
  * @LastEditors: MRNAN
  * @FilePath: /Vue-ebook-pro/src/components/store/SearchBar.vue
 -->
@@ -18,10 +18,12 @@
         </div>
       </div>
     </transition>
-    <div class="title-icon-back-wrapper">
+    <div class="title-icon-back-wrapper" :class="{'title-hidden':!titleVisible}">
       <span class="icon-back"></span>
     </div>
     <div class="search-bar-input-wrapper" :class="{'title-hidden':!titleVisible}">
+      <!-- 占位，占据左侧返回按钮的位置大小，主要是在隐藏title的时候压缩input框 -->
+      <div class="search-bar-blank" :class="{'title-hidden':!titleVisible}"></div>
       <div class="search-bar-input">
         <span class="icon-search"></span>
         <input type="text" :placeholder="$t('home.hint')" />
@@ -94,20 +96,32 @@ export default {
     top: 0;
     height: px2rem(42);
     @include center;
+    transition: height .2s linear;
+    &.title-hidden{
+      height: px2rem(52);
+    }
   }
   .search-bar-input-wrapper {
     width: 100%;
     position: absolute;
     left: 0;
     top: px2rem(42);
-    background: white;
     padding: px2rem(10);
     box-sizing: border-box;
     transition: top .2s linear;
+    display: flex;
     &.title-hidden{
       top: 0;
     }
+    .search-bar-blank{
+      flex: 0;
+      transition: all .2s linear;
+      &.title-hidden{
+        flex: 0 0 px2rem(31);
+      }
+    }
     .search-bar-input {
+      flex: 1;
       background: #f4f4f4;
       border-radius: px2rem(20);
       padding: px2rem(5) px2rem(15);
